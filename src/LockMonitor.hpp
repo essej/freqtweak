3a38e34f084b02610ceabb2f10937e85ef091274
Yann Collette <ycollette.nospam@free.fr>
2019-08-03 21:56:47 +0200

    fix fsf address

--- src/LockMonitor.hpp
+++ src/LockMonitor.hpp
@@ -13,7 +13,8 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+    MA 02110-1301 USA.
 
     $Id: LockMonitor.hpp,v 1.2 2004/04/22 08:16:16 essej Exp $
 */
--- src/RingBuffer.cpp
+++ src/RingBuffer.cpp
@@ -13,7 +13,8 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+    MA 02110-1301 USA.
 
     $Id: RingBuffer.cpp,v 1.2 2004/11/10 17:16:08 trutkin Exp $
 */
